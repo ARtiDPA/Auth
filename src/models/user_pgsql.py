5cc307590528a802d028dcef920c64f12a0c7f3b
Artyom Danilov <artemdancte@mail.ru>
2024-03-06 22:28:18 +0300

    add[models]: добавил модель user для храния данных в postgresql

---
+++ src/models/user_pgsql.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer
+from db import postgresql
+
+
+class user(postgresql.Base_pgsql):
+
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    surname: Mapped[str] = mapped_column(String)
+    age: Mapped[int] = mapped_column(Integer)
+    mail: Mapped[str] = mapped_column(String)
+    number: Mapped[int] = mapped_column(Integer)
+    passwrod: Mapped[str] = mapped_column(String)
