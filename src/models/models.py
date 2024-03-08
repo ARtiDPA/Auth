1c468c271ade9fae333906b48251991ac5b9f28c
Artyom Danilov <artemdancte@mail.ru>
2024-03-09 00:15:46 +0300

    add[models]: добавил модель user

---
+++ src/models/models.py
@@ -0,0 +1,23 @@
+"""Модели на sqlalhemy для pgsql."""
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from db import postgresql
+
+
+class User(postgresql.Base_pgsql):
+    """Модель пользователя.
+
+    Args:
+        postgresql (config): Базоый декларотивный класс.
+    """
+
+    __tablename__ = 'user'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    surname: Mapped[str] = mapped_column(String)
+    age: Mapped[int] = mapped_column(Integer)
+    mail: Mapped[str] = mapped_column(String)
+    number: Mapped[int] = mapped_column(Integer)
+    passwrod: Mapped[str] = mapped_column(String)
