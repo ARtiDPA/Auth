658463b1822fff035872cf378ade3da3f0112aae
Artyom Danilov <artemdancte@mail.ru>
2024-03-09 20:17:49 +0300

    delete[file]: удалил файлы

--- src/db/db_pgsql.py
+++
@@ -1,9 +0,0 @@
-"""Создание движка для PgSQL."""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-
-from src.core.config_pgsql import settings_pgsql
-
-Base_pgsql = declarative_base()
-
-engine_pgsql = create_engine(url=settings_pgsql)
