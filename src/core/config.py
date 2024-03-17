164b3236699c812ad89d5f3478a46cb2e912cbea
Artyom Danilov <artemdancte@mail.ru>
2024-03-17 11:50:01 +0300

    feet[config]: исправил файл настроек

--- src/core/config.py
+++ src/core/config.py
@@ -1,50 +1,23 @@
-"""Настройки."""
+"""Файл настроек."""
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class SettingsPGsql(BaseSettings):
-    """Настройки ссылки.
+class DatabaseSettings(BaseSettings):
+    """Настройки бд.
 
     Args:
-        BaseSettings (config): Базовый декларативный класс.
-
-    Returns:
-        : Ссылка на движёк для работы с бд
+        BaseSettings (class): базовые настройки
     """
 
+    model_config = SettingsConfigDict(env_file='.env')
+
+    db_driver: str
     db_host: str
     db_port: int
     db_user: str
     db_password: str
     db_name: str
-
-    def data_base_pgsql(self) -> str:
-        """Создание ссылки.
-
-        Args:
-            None
-
-        Returns:
-            str: Ссылка на движек.
-        """
-        link_engine = """asyncpg://
-            {db_user}:
-            {db_password}@
-            {db_host}:
-            {db_port}/
-            {db_name}"""
-
-        link_engine.format(
-            db_user=self.db_user,
-            db_password=self.db_password,
-            db_host=self.db_host,
-            db_port=self.db_port,
-            db_name=self.db_name,
-            )
-
-        return link_engine
-
-    model_config = SettingsConfigDict(env_file='.env')
+    db_schema: str
 
 
-settings_pgsql = SettingsPGsql()
+database_settings = DatabaseSettings()
