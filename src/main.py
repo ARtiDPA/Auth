573304c3bbf11ecbe3feff100f0cb1be8c4d2fae
ARtiDPA <artemdancte@mail.ru>
2024-12-12 13:29:45 +0300

    fix[start]: изменил точку входа и исправил баг с PGsql

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 """Точка входа системы."""
-import uvicorn
 from fastapi import FastAPI
 
 from routes.system.route import app as route_three
@@ -11,6 +10,3 @@ app = FastAPI()
 app.include_router(router=route, tags=['User'])
 app.include_router(router=route_two, tags=['Tokens'])
 app.include_router(router=route_three, tags=['System'])
-
-if __name__ == '__main__':
-    uvicorn.run(app, port=8000)
